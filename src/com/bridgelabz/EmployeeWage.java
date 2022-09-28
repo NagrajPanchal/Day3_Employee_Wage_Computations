0a0368634d32b81401befcc6167747397a1dd8bc
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-28 21:54:09 +0530

    Add : UC4 Employee Wage By using Switch Case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,27 +5,26 @@ public class EmployeeWage
 {
     private static void employeeFullPartTime()
     {
-        int EMPLOYEE_PRESENT = 1; // Variable initialization
+        final int EMPLOYEE_FULL_TIME = 2; // Variable initialization
+        final int EMPLOYEE_PART_TIME = 1;
         int checkEmployee = (int) ((Math.floor(Math.random()* 10)) % 3);
-        int empWagePerHour =20;
-        int fullTimeHour = 8;
-        int partTimeHour = 4;
-        int fullDayWage = empWagePerHour * fullTimeHour;
-        int partDayWage = empWagePerHour * partTimeHour;
-        if(checkEmployee == EMPLOYEE_PRESENT)
+        final int EMP_WAGE_PER_HOUR =20;
+        final int FULL_TIME_HOUR = 8;
+        final int PART_TIME_HOUR = 4;
+        int fullDayWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
+        int partDayWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
+        switch (checkEmployee)
         {
-            System.out.println("Employee is Full Present & Daily Wage : " +fullDayWage);
+            case EMPLOYEE_FULL_TIME :
+                System.out.println("Employee is Full Present & Daily Wage : " +fullDayWage);
+                break;
+            case EMPLOYEE_PART_TIME :
+                System.out.println("Employee is Part Time Present & Daily Wage : " +partDayWage);
+                break;
+            default :
+                System.out.println("Employee is Absent");
         }
-        else if (checkEmployee !=0)
-        {
-            System.out.println("Employee is Part Time Present & Daily Wage : " +partDayWage);
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
-        }
-
-    }
+ }
     public static void main(String[] args)
     {
         employeeFullPartTime();
