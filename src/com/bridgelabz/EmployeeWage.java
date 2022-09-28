a19b5c6eae951406c42d043f8b82131a3fa0e874
Nagraj Panchal <109675842+NagrajPanchal@users.noreply.github.com>
2022-09-28 20:23:32 +0530

    Update EmployeeWage.java
    Comments

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,16 @@
 // UC2 Employee Day Wage
 
-package com.bridgelabz;
-public class EmployeeWage
+package com.bridgelabz; // Package
+public class EmployeeWage // Class
 {
-    private static void employeeDayWage()
+    private static void employeeDayWage() // Methode
     {
         int EMPLOYEE_PRESENT = 1; // Variable initialization
         int checkEmployee = (int) ((Math.floor(Math.random()* 10)) % 2);
         int empWagePerHour =20;
         int dayHours = 8;
         int dayWage = empWagePerHour * dayHours;
-        if(checkEmployee == EMPLOYEE_PRESENT)
+        if(checkEmployee == EMPLOYEE_PRESENT) // if Condition
         {
             System.out.println("Employee is Present");
             System.out.println("Employee Daily Wage : " +dayWage);
@@ -21,8 +21,8 @@ public class EmployeeWage
         }
 
     }
-    public static void main(String[] args)
+    public static void main(String[] args) // main method
     {
-        employeeDayWage();
+        employeeDayWage(); // call to method
     }
 }
