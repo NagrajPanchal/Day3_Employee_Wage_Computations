21ead23ec726dcf7e55cd7e86b89635360210fa6
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-29 17:29:47 +0530

    Update : UC1 Employee Attendance Present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,48 +1,31 @@
-// UC6 Employee Calculate Total Working Hours and day wages
+// UC1 Employee Attendance
 
-package com.bridgelabz;
-public class EmployeeWage
+package com.bridgelabz;  //Package
+public class EmployeeWage // Class
 {
-        private static void employeeCalculateHours()
+    private static void employeePresentAbsent() //Method
+    {
+        int EMPLOYEE_PRESENT = 1; // Variable initialization
+        /*
+        double random =  Math.random(); // Random number generating 0 to 1
+        double random1 = random * 10; // 1 to 10 number random number generating
+        double floor = Math.floor(random1); // Floor function will generate around values
+        int floor1 = (int) (floor); // Type Casting because of single digit value
+        int var = floor1 % 2; // Mod 2 reminder will produce 0 or 1
+        */
+        int var = (int) ((Math.floor(Math.random()* 10)) % 2);
+
+        if(var == EMPLOYEE_PRESENT) // if Condition
         {
-            final int EMPLOYEE_FULL_TIME = 2; // Variable initialization
-            final int EMPLOYEE_PART_TIME = 1;
-            final int EMP_WAGE_PER_HOUR =20;
-            final int FULL_TIME_HOUR = 8;
-            final int PART_TIME_HOUR = 4;
-            int day = 0;
-            int monthWages = 0;
-            int dayWage = 0;
-            int totalHours = 0;
-            if (totalHours < 100 && day < 20) {
-                for (day = 0; day <= 20; day++) {
-                    int checkEmployee = (int) ((Math.floor(Math.random() * 10)) % 3);
-                    switch (checkEmployee) {
-                        case EMPLOYEE_FULL_TIME:
-                            dayWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
-                            totalHours = totalHours + FULL_TIME_HOUR;
-                            System.out.println("Employee day " + day + " is Full Present & Daily Wage : " + dayWage + " & Hours " + totalHours);
-                            break;
-                        case EMPLOYEE_PART_TIME:
-                            dayWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
-                            totalHours = totalHours + PART_TIME_HOUR;
-                            System.out.println("Employee day " + day + " is Part Time Present & Daily Wage : " + dayWage + " & Hours " + totalHours);
-                            break;
-                        default:
-                            int isAbsent = 0;
-                            System.out.println("Employee day " + day + " is Absent & Daily Wage : " + isAbsent);
-                    }
-                    monthWages = monthWages + dayWage;
-                }
-                System.out.println("Employee per Month Wages : " + monthWages + " & Total Hours " + totalHours);
-            }
-            else
-            {
-                System.out.println("Employee per Month Wages : " + monthWages + " & Total Hours " + totalHours);
-            }
+            System.out.println("Employee is Present");
         }
-        public static void main(String[] args)
+        else
         {
-            employeeCalculateHours();
+            System.out.println("Employee is Absent");
         }
     }
+    public static void main(String[] args) // Main
+    {
+        employeePresentAbsent(); // call method
+    }
+}
